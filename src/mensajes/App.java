2e1a762902645a844baadec9916c0a4f582f93e5
Brais <bgelabert19547@iesjoanramis.org>
2026-01-08 18:15:50 +0100

    Added despedirse method

--- src/mensajes/App.java
+++ src/mensajes/App.java
@@ -3,9 +3,14 @@ package mensajes;
 public class App {
     public static void main(String[] args) {
         mostrarMensaje();
+        despedirse();
     }
     
     static void mostrarMensaje() {
         System.out.println("Bienvenido a la aplicación");
+    }
+    
+    static void despedirse() {
+    	System.out.println("Hasta pronto");
     }
 }
